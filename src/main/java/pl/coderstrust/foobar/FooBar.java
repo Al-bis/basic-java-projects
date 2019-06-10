4c15defcf961ebfc32fb2606a3b3b5aa39f5424a
Al-bis <albert.brandt123@gmail.com>
2019-06-10 20:08:09 +0200

    fooBar change

--- src/main/java/pl/coderstrust/foobar/FooBar.java
+++ src/main/java/pl/coderstrust/foobar/FooBar.java
@@ -4,18 +4,26 @@ public class FooBar {
 
     public static void main(String[] args) {
 
-        for(int i=0; i<101; i++) {
-            System.out.print(i + " ");
+        printFooBar(101);
+    }
 
-            if(i%3 == 0) {
-                System.out.print("Foo");
+    private static void printFooBar(int amount) {
+
+        StringBuilder fooBar = new StringBuilder();
+
+        for(int i = 0; i < amount; i++) {
+            fooBar.append(i).append(" ");
+
+            if(i % 3 == 0) {
+                fooBar.append("Foo");
             }
 
-            if(i%5 == 0) {
-                System.out.print("Bar");
+            if(i % 5 == 0) {
+                fooBar.append("Bar");
             }
 
-            System.out.print("\n");
+            System.out.println(fooBar.toString());
+            fooBar.setLength(0);
         }
     }
 }
