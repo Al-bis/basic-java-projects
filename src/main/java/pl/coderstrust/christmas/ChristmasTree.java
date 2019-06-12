4405e0a833d62fc11aff4f77fa8f2983913d4570
Al-bis <albert.brandt123@gmail.com>
2019-06-12 18:53:38 +0200

    Empty lines removed, printChristmasTree method refactored, class renamed

--- src/main/java/pl/coderstrust/christmas/ChristmasTree.java
+++ src/main/java/pl/coderstrust/christmas/ChristmasTree.java
@@ -1,26 +1,23 @@
 package pl.coderstrust.christmas;
 
-public class Christmas {
+public class ChristmasTree {
 
     public static void main(String[] args) {
-
         printChristmasTree(6);
     }
 
     public static void printChristmasTree(int size) {
-
+        int star = 1;
         for (int i = 0; i < size; i++) {
-
-            for (int j = 1; j < size * 2; j++) {
-
-                if (j >= (size - i) && j <= (size + i)) {
-                    System.out.print("*");
-                    continue;
-                }
+            for (int j = 0; j < size - i; j++) {
                 System.out.print(" ");
             }
+            for (int j = 0; j < star; j++) {
+                System.out.print("*");
+            }
+            star += 2;
             System.out.println();
         }
-        System.out.printf("%" + size + "s", "**");
+        System.out.printf("%" + (size + 1) + "s", "**");
     }
 }
