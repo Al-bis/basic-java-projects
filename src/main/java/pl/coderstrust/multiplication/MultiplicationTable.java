a6db702d88e3891df3bb718ce0419ba44e3124c6
Al-bis <albert.brandt123@gmail.com>
2019-06-15 20:55:34 +0200

    Create two if statements, remove empty lines.

--- src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
+++ src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
@@ -11,26 +11,23 @@ public class MultiplicationTable {
     }
 
     public static void printMultiplicationTable(int size) {
-        if (size < 0 || size > 12) {
-            throw new IllegalArgumentException("Given size must be between 0 and 12");
+        if (size < 0) {
+            throw new IllegalArgumentException("Given size can't have a negative value");
         }
-
-        System.out.printf("%6s", " ");
-
+        if (size > 12) {
+            throw new IllegalArgumentException("Given size can't exceed 12");
+        }
+        System.out.printf("%6s", "");
         for (int i = 1; i <= size; i++) {
             System.out.printf("%6d", i);
         }
-
         System.out.println();
-
         for (int i = 1; i <= size; i++) {
             System.out.printf("%6d", i);
-
             for (int j = 1; j <= size; j++) {
                 System.out.printf("%6d", i * j);
             }
             System.out.println();
         }
-
     }
 }
