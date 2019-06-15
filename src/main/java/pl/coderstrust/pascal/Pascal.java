da190525a50c4cce4d663d4309e0fd80d0a9115e
Al-bis <albert.brandt123@gmail.com>
2019-06-15 10:22:10 +0200

    Remove empty lines, rename variables.

--- src/main/java/pl/coderstrust/pascal/Pascal.java
+++ src/main/java/pl/coderstrust/pascal/Pascal.java
@@ -3,39 +3,31 @@ package pl.coderstrust.pascal;
 public class Pascal {
 
     public static void main(String[] args) {
-
         printPascalTriangle(5);
     }
 
     public static void printPascalTriangle(int rows) {
-
-        long term;
+        long nodeValue;
         for (int i = 0; i < rows; i++) {
 
             for (int j = i; j <= rows; j++) {
-
                 System.out.printf("%3s", " ");
             }
 
-            for (int k = 0; k <= i; k++) {
-
-                term = fact(i) / (fact(k) * fact(i - k));
-                System.out.printf("%6d", term);
+            for (int j = 0; j <= i; j++) {
+                nodeValue = factorial(i) / (factorial(j) * factorial(i - j));
+                System.out.printf("%6d", nodeValue);
             }
-
             System.out.println();
         }
     }
 
-    private static long fact(int input) {
-
-        long factorial = 1L;
-        while (input >= 1) {
-
-            factorial *= input;
-            input--;
+    private static long factorial(int number) {
+        long result = 1L;
+        while (number >= 1) {
+            result *= number;
+            number--;
         }
-
-        return factorial;
+        return result;
     }
 }
