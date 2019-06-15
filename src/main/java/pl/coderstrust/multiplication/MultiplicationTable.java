e4827baa74f3fe72a55b9b0b2f2b920e493a8518
Al-bis <albert.brandt123@gmail.com>
2019-06-15 10:09:00 +0200

    Remove empty lines, add IllegalArgumentException

--- src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
+++ src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
@@ -3,36 +3,32 @@ package pl.coderstrust.multiplication;
 public class MultiplicationTable {
 
     public static void main(String[] args) {
-
-        printMultiplicationTable(4);
+        try {
+            printMultiplicationTable(4);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public static void printMultiplicationTable(int size) {
-
-        if(size > 12) {
-            System.out.println("Given size can't be more then 12");
-            return;
+        if (size < 0 || size > 12) {
+            throw new IllegalArgumentException("Given size must be between 0 and 12");
         }
 
-        System.out.print("      ");
+        System.out.printf("%6s", " ");
 
-        for(int i = 1; i <= size; i++) {
-
+        for (int i = 1; i <= size; i++) {
             System.out.printf("%6d", i);
-
         }
 
         System.out.println();
 
-        for(int i = 1; i <= size; i++) {
-
+        for (int i = 1; i <= size; i++) {
             System.out.printf("%6d", i);
 
-            for(int j = 1; j <= size; j++) {
-
-                System.out.printf("%6d", i*j);
+            for (int j = 1; j <= size; j++) {
+                System.out.printf("%6d", i * j);
             }
-
             System.out.println();
         }
 
