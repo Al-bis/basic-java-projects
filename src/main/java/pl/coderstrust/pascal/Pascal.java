28d3633b9110bcc7b0ae3ab380610111fbe6ae59
Al-bis <albert.brandt123@gmail.com>
2019-06-15 20:34:27 +0200

    Remove empty lines.

--- src/main/java/pl/coderstrust/pascal/Pascal.java
+++ src/main/java/pl/coderstrust/pascal/Pascal.java
@@ -9,11 +9,9 @@ public class Pascal {
     public static void printPascalTriangle(int rows) {
         long nodeValue;
         for (int i = 0; i < rows; i++) {
-
             for (int j = i; j <= rows; j++) {
                 System.out.printf("%3s", " ");
             }
-
             for (int j = 0; j <= i; j++) {
                 nodeValue = factorial(i) / (factorial(j) * factorial(i - j));
                 System.out.printf("%6d", nodeValue);
