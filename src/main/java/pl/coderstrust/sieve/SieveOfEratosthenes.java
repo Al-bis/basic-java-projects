ac0cf267cf37630164130afa113da5a5f5b3e6e6
Al-bis <albert.brandt123@gmail.com>
2019-06-20 21:00:46 +0200

    Extracted part of code to separately functions.

--- src/main/java/pl/coderstrust/sieve/SieveOfEratosthenes.java
+++ src/main/java/pl/coderstrust/sieve/SieveOfEratosthenes.java
@@ -5,35 +5,52 @@ import java.util.Arrays;
 public class SieveOfEratosthenes {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(sieve(12)));
+        System.out.println(Arrays.toString(sieve(122)));
     }
 
     public static int[] sieve(int maximumNumber) {
-        boolean[] numbers = new boolean[maximumNumber + 1];
-        for (int i = 2; i <= maximumNumber; i++) {
-            numbers[i] = true;
+        if (maximumNumber < 2) {
+            return new int[0];
         }
-        for (int i = 2; i <= Math.sqrt(maximumNumber); i++) {
-            if (numbers[i] == true) {
-                for (int j = (int) Math.pow(i, 2); j <= maximumNumber; j = j + i) {
-                    numbers[j] = false;
+        int[] array = createInitialArray(maximumNumber);
+        int countOfPrimeNumbers = markNonePrimeNumbers(array);
+        return extractPrimesNumbers(array, countOfPrimeNumbers);
+    }
+
+    private static int markNonePrimeNumbers(int[] array) {
+        for (int i = 2; i <= Math.sqrt(array.length - 1); i++) {
+            if (array[i] == 1) {
+                for (int j = (int) Math.pow(i, 2); j <= array.length - 1; j = j + i) {
+                    array[j] = 0;
                 }
             }
         }
-        int primarysAmount = 0;
-        for (int i = 0; i <= maximumNumber; i++) {
-            if (numbers[i] == true) {
-                primarysAmount++;
+        int primaryAmount = 0;
+        for (int i = 0; i <= array.length - 1; i++) {
+            if (array[i] == 1) {
+                primaryAmount++;
             }
         }
-        int[] primaryNumbers = new int[primarysAmount];
+        return primaryAmount;
+    }
+
+    private static int[] extractPrimesNumbers(int[] array, int countOfPrimeNumbers) {
+        int[] primaryNumbers = new int[countOfPrimeNumbers];
         int position = 0;
-        for (int i = 0; i <= maximumNumber; i++) {
-            if (numbers[i] == true) {
+        for (int i = 0; i <= array.length - 1; i++) {
+            if (array[i] == 1) {
                 primaryNumbers[position] = i;
                 position++;
             }
         }
         return primaryNumbers;
+    }
+
+    private static int[] createInitialArray(int maximumNumber) {
+        int[] numbers = new int[maximumNumber + 1];
+        for (int i = 2; i <= maximumNumber; i++) {
+            numbers[i] = 1;
+        }
+        return numbers;
     }
 }
