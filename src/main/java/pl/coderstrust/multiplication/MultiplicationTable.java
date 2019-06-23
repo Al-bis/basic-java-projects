cb3f4117b0e3361addb3d52c42af612a142b60a7
Al-bis <albert.brandt123@gmail.com>
2019-06-23 11:27:56 +0200

    Added List to getMultiplicationTable, added tests.

--- src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
+++ src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
@@ -1,33 +1,44 @@
 package pl.coderstrust.multiplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MultiplicationTable {
 
     public static void main(String[] args) {
         try {
-            printMultiplicationTable(4);
+            List<String> multiplicationTable = getMultiplicationTable(7);
+            for (String s : multiplicationTable) {
+                System.out.println(s);
+            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public static void printMultiplicationTable(int size) {
+    public static List<String> getMultiplicationTable(int size) {
+        List<String> result = new ArrayList<>();
+        StringBuilder line = new StringBuilder();
         if (size < 0) {
             throw new IllegalArgumentException("Given size cannot have a negative value");
         }
         if (size > 12) {
             throw new IllegalArgumentException("Given size cannot exceed 12");
         }
-        System.out.printf("%6s", "");
+        line.append(String.format("%6s", ""));
         for (int i = 1; i <= size; i++) {
-            System.out.printf("%6d", i);
+            line.append(String.format("%6d", i));
         }
-        System.out.println();
+        result.add(line.toString());
+        line.setLength(0);
         for (int i = 1; i <= size; i++) {
-            System.out.printf("%6d", i);
+            line.append(String.format("%6d", i));
             for (int j = 1; j <= size; j++) {
-                System.out.printf("%6d", i * j);
+                line.append(String.format("%6d", i * j));
             }
-            System.out.println();
+            result.add(line.toString());
+            line.setLength(0);
         }
+        return result;
     }
 }
---
+++ src/test/java/pl/coderstrust/multiplication/MultiplicationTableTest.java
@@ -0,0 +1,62 @@
+package pl.coderstrust.multiplication;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+@RunWith(JUnitParamsRunner.class)
+public class MultiplicationTableTest {
+
+    @Test
+    @Parameters(method = "inputDataException")
+    public void shouldThrownException(int size) {
+        //given
+        Exception caughtException = null;
+        //when
+        try {
+            MultiplicationTable.getMultiplicationTable(size);
+        } catch (Exception e) {
+            caughtException = e;
+        }
+        //then
+        assertEquals(IllegalArgumentException.class, caughtException.getClass());
+    }
+
+    @Test
+    @Parameters(method = "inputData")
+    public void shouldReturnCorrectMultiplicationTable(int size, List<String> expected) {
+        //given
+        //when
+        List<String> actual = MultiplicationTable.getMultiplicationTable(size);
+        //then
+        assertThat(actual, is(expected));
+    }
+
+    public Object[] inputDataException() {
+        return new Object[]{
+                new Object[]{-1},
+                new Object[]{13}
+        };
+    }
+
+    public Object[] inputData() {
+        return new Object[]{
+                new Object[]{4, Arrays.asList("           1     2     3     4",
+                        "     1     1     2     3     4",
+                        "     2     2     4     6     8",
+                        "     3     3     6     9    12",
+                        "     4     4     8    12    16")},
+                new Object[]{2, Arrays.asList("           1     2",
+                        "     1     1     2",
+                        "     2     2     4")}
+        };
+    }
+}
