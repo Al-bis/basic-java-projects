5517bc975cb13841fbddc3061bc0562c39f611ee
Al-bis <albert.brandt123@gmail.com>
2019-06-24 17:35:40 +0200

    Changed way of testing the exception case.

--- src/test/java/pl/coderstrust/multiplication/MultiplicationTableTest.java
+++ src/test/java/pl/coderstrust/multiplication/MultiplicationTableTest.java
@@ -2,32 +2,28 @@ package pl.coderstrust.multiplication;
 
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 @RunWith(JUnitParamsRunner.class)
 public class MultiplicationTableTest {
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     @Test
     @Parameters(method = "inputDataException")
     public void shouldThrownException(int size) {
-        //given
-        Exception caughtException = null;
-        //when
-        try {
-            MultiplicationTable.getMultiplicationTable(size);
-        } catch (Exception e) {
-            caughtException = e;
-        }
-        //then
-        assertEquals(IllegalArgumentException.class, caughtException.getClass());
+        thrown.expect(IllegalArgumentException.class);
+        MultiplicationTable.getMultiplicationTable(size);
     }
 
     @Test
@@ -49,12 +45,14 @@ public class MultiplicationTableTest {
 
     public Object[] inputData() {
         return new Object[]{
-                new Object[]{4, Arrays.asList("           1     2     3     4",
+                new Object[]{4, Arrays.asList(
+                        "           1     2     3     4",
                         "     1     1     2     3     4",
                         "     2     2     4     6     8",
                         "     3     3     6     9    12",
                         "     4     4     8    12    16")},
-                new Object[]{2, Arrays.asList("           1     2",
+                new Object[]{2, Arrays.asList(
+                        "           1     2",
                         "     1     1     2",
                         "     2     2     4")}
         };
