07d2ae7ba2bb128d8c9882b979d88ba549e91c86
Al-bis <albert.brandt123@gmail.com>
2019-06-24 17:52:49 +0200

    Changed way of testing the exception case.

--- src/test/java/pl/coderstrust/pascal/PascalTest.java
+++ src/test/java/pl/coderstrust/pascal/PascalTest.java
@@ -2,32 +2,28 @@ package pl.coderstrust.pascal;
 
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 @RunWith(JUnitParamsRunner.class)
 public class PascalTest {
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     @Test
     @Parameters(method = "inputDataException")
     public void shouldThrownException(int rows) {
-        //given
-        Exception caughtException = null;
-        //when
-        try {
-            Pascal.getPascalTriangle(rows);
-        } catch (Exception e) {
-            caughtException = e;
-        }
-        //then
-        assertEquals(IllegalArgumentException.class, caughtException.getClass());
+        thrown.expect(IllegalArgumentException.class);
+        Pascal.getPascalTriangle(rows);
     }
 
     @Test
@@ -48,12 +44,14 @@ public class PascalTest {
 
     public Object[] inputData() {
         return new Object[]{
-                new Object[]{5, Arrays.asList("                       1",
+                new Object[]{5, Arrays.asList(
+                        "                       1",
                         "                    1     1",
                         "                 1     2     1",
                         "              1     3     3     1",
                         "           1     4     6     4     1")},
-                new Object[]{3, Arrays.asList("                 1",
+                new Object[]{3, Arrays.asList(
+                        "                 1",
                         "              1     1",
                         "           1     2     1")}
         };
