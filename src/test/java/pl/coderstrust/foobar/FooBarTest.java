ed267aa2b285640378e85d97bf70fe38166593d7
Al-bis <albert.brandt123@gmail.com>
2019-06-27 19:54:39 +0200

    Added suggested changes in test class.

--- src/test/java/pl/coderstrust/foobar/FooBarTest.java
+++ src/test/java/pl/coderstrust/foobar/FooBarTest.java
@@ -2,44 +2,41 @@ package pl.coderstrust.foobar;
 
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 @RunWith(JUnitParamsRunner.class)
 public class FooBarTest {
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     @Test
-    public void shouldThrownException() {
-        //given
-        Exception caughtException = null;
-        //when
-        try {
-            FooBar.getFooBar(-1);
-        } catch (Exception e) {
-            caughtException = e;
-        }
-        //then
-        assertEquals(IllegalArgumentException.class, caughtException.getClass());
+    @Parameters({"-1", "-2", "-3"})
+    public void shouldThrowExceptionForInvalidArgument(int amount) {
+        thrown.expect(IllegalArgumentException.class);
+        FooBar.getFooBar(amount);
     }
 
     @Test
-    @Parameters(method = "inputData")
+    @Parameters(method = "fooBarArguments")
     public void shouldReturnCorrectFooBar(int amount, List<String> expected) {
-        //given
         //when
         List<String> actual = FooBar.getFooBar(amount);
+
         //then
         assertThat(actual, is(expected));
     }
 
-    public Object[] inputData() {
+    public Object[] fooBarArguments() {
         return new Object[]{
                 new Object[]{10, Arrays.asList("0 FooBar", "1 ", "2 ", "3 Foo", "4 ", "5 Bar", "6 Foo", "7 ", "8 ", "9 Foo")},
                 new Object[]{5, Arrays.asList("0 FooBar", "1 ", "2 ", "3 Foo", "4 ")},
