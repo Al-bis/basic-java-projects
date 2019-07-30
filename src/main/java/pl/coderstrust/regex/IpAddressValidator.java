2e7a5e8346de5f48e795a2e594ff04ae48950dff
Al-bis <albert.brandt123@gmail.com>
2019-07-30 19:29:36 +0200

    Added suggested changes.

--- src/main/java/pl/coderstrust/regex/IpAddressValidator.java
+++ src/main/java/pl/coderstrust/regex/IpAddressValidator.java
@@ -2,20 +2,19 @@ package pl.coderstrust.regex;
 
 import java.util.regex.Pattern;
 
-public class IPValidator {
+public class IpAddressValidator {
 
-    private Pattern pattern;
-    private static final String IP_PATTERN =
+    private static final String IP_ADDRESS_PATTERN =
             "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
                     "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
                     "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
                     "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+    private static Pattern pattern = Pattern.compile(IP_ADDRESS_PATTERN);
 
-    public IPValidator() {
-        pattern = Pattern.compile(IP_PATTERN);
-    }
-
-    public boolean isIpAddress(String input) {
+    public static boolean isIpAddress(String input) {
+        if (input == null) {
+            throw new NullPointerException();
+        }
         return pattern.matcher(input).matches();
     }
 }
