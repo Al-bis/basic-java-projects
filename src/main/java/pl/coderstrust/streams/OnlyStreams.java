a3c18ae86d72faa6d9712268b9ebe4884302e44f
Al-bis <albert.brandt123@gmail.com>
2019-07-30 21:11:29 +0200

    Implemented Do you love streams task 26.

--- src/main/java/pl/coderstrust/streams/OnlyStreams.java
+++ src/main/java/pl/coderstrust/streams/OnlyStreams.java
@@ -4,19 +4,34 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.regex.Pattern;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class OnlyStreams {
 
     public static void main(String[] args) throws IOException {
 
-        Path path = Paths.get("src/main/resources/1000.txt");
-        Files.lines(path)
+        Path input = Paths.get("src/main/resources/1000.txt");
+        Path result = Paths.get("src/main/resources/result.txt");
+        List<String> list = Files.lines(input)
                 .filter(line -> line.matches("^[ 0-9]+$"))
-                //.map(string -> string.split(" +"))
-                .flatMap(Pattern.compile(" +")::splitAsStream)
-                .reduce("+", String::concat);
-        //.collect(Collectors.joining("+"))
-        //.forEach(System.out::println);
+                .map(string -> string.split(" +"))
+                .map(OnlyStreams::convert)
+                .collect(Collectors.toList());
+        Files.write(result, list);
+    }
+
+    private static String convert(String[] array) {
+        String joined = Arrays.stream(array)
+                .filter(s -> s.length() > 0)
+                .reduce("", (partialString, element) -> partialString + "+" + element);
+
+        int sum = Arrays.stream(array)
+                .filter(s -> s.length() > 0)
+                .mapToInt(num -> Integer.parseInt(num))
+                .sum();
+
+        return joined + "=" + sum;
     }
 }
