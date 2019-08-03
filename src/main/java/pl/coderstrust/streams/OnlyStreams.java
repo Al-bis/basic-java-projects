23e9c50b5a646eec583a1260179ed9739dbb7031
Al-bis <albert.brandt123@gmail.com>
2019-08-03 14:29:46 +0200

    Added suggested change.

--- src/main/java/pl/coderstrust/streams/OnlyStreams.java
+++ src/main/java/pl/coderstrust/streams/OnlyStreams.java
@@ -1,25 +1,24 @@
 package pl.coderstrust.streams;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class OnlyStreams {
 
     public static void main(String[] args) throws IOException {
-
         Path input = Paths.get("src/main/resources/1000.txt");
         Path result = Paths.get("src/main/resources/result.txt");
-        List<String> list = Files.lines(input)
-                .filter(line -> line.matches("^[ 0-9]+$"))
-                .map(string -> string.split(" +"))
-                .map(OnlyStreams::convert)
-                .collect(Collectors.toList());
-        Files.write(result, list);
+        try (PrintWriter pw = new PrintWriter(Files.newBufferedWriter(result))) {
+            Files.lines(input)
+                    .filter(line -> line.matches("^[ 0-9]+$"))
+                    .map(string -> string.split(" +"))
+                    .map(OnlyStreams::convert)
+                    .forEach(row -> pw.write(row + "\n"));
+        }
     }
 
     private static String convert(String[] array) {
@@ -29,9 +28,9 @@ public class OnlyStreams {
 
         int sum = Arrays.stream(array)
                 .filter(s -> s.length() > 0)
-                .mapToInt(num -> Integer.parseInt(num))
+                .mapToInt(Integer::parseInt)
                 .sum();
 
-        return joined + "=" + sum;
+        return String.format("%s=%d", joined, sum);
     }
 }
