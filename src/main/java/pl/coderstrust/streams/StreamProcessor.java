48bfddbe6cfff62b26131707065cb078a3bffb6e
Al-bis <albert.brandt123@gmail.com>
2019-08-03 15:07:32 +0200

    Extracted code from main to separate method.

--- src/main/java/pl/coderstrust/streams/StreamProcessor.java
+++ src/main/java/pl/coderstrust/streams/StreamProcessor.java
@@ -3,17 +3,26 @@ package pl.coderstrust.streams;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class StreamProcessor {
 
     public static void main(String[] args) throws IOException {
-        Path input = Paths.get("src/main/resources/1000.txt");
-        Path result = Paths.get("src/main/resources/result.txt");
-        try (PrintWriter pw = new PrintWriter(Files.newBufferedWriter(result))) {
-            Files.lines(input)
+        String input = "src/main/resources/1000.txt";
+        String result = "src/main/resources/result.txt";
+        process(input, result);
+    }
+
+    public static void process(String inputFilePath, String resultFilePath) throws IOException {
+        if (inputFilePath == null) {
+            throw new IllegalArgumentException("Argument cannot be null.");
+        }
+        if (resultFilePath == null) {
+            throw new IllegalArgumentException("Argument cannot be null.");
+        }
+        try (PrintWriter pw = new PrintWriter(Files.newBufferedWriter(Paths.get(resultFilePath)))) {
+            Files.lines(Paths.get(inputFilePath))
                     .filter(line -> line.matches("^[ 0-9]+$"))
                     .map(string -> string.split(" +"))
                     .map(StreamProcessor::convert)
